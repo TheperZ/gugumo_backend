8552f1f944a6d5badd01dd2c543d40bf08e47c77
TheperZ <ckdgh7884@gmail.com>
2025-06-22 15:30:55 +0900

    CustomException 구조 수정

--- src/main/java/sideproject/gugumo/exception/exception/NotFoundException.java
+++ src/main/java/sideproject/gugumo/exception/exception/NotFoundException.java
@@ -2,8 +2,8 @@ package sideproject.gugumo.exception.exception;
 
 import sideproject.gugumo.response.StatusCode;
 
-public class NotFoundException extends RuntimeException {
+public class NotFoundException extends CustomException {
     public NotFoundException(StatusCode statusCode) {
-        super(statusCode.getCustomMessage());
+        super(statusCode);
     }
 }
